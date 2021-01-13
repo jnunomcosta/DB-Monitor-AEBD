fbb111d624e5322656c36e8b9882b9f04a7daa80
João Diogo Mota <36596261+JoaoDiogoMota@users.noreply.github.com>
2021-01-13 00:52:58 +0000

    Add files via upload

--- Trabalho Prático/Interface/dbmonitor/api/routes/index.js
+++ Trabalho Prático/Interface/dbmonitor/api/routes/index.js
@@ -16,6 +16,41 @@ router.get('/users',(req,res,next) =>{
       .catch(erro => res.jsonp(erro))
   })
 
+  router.get('/tablespaces',(req,res) =>{
+    connection.getTablespaces()
+      .then(dados => res.jsonp(dados)) 
+      .catch(erro => res.jsonp(erro))
+  })
+  
+  router.post('/timestamp',(req,res) =>{
+    connection.getTimeStamps(req.body.table)
+      .then(dados => res.jsonp(dados)) 
+      .catch(erro => res.jsonp(erro))
+  })
+
+  router.get('/tablespacedata/:id/:id2',(req,res) =>{
+    connection.getTablespaceData(req.params.id,req.params.id2)
+      .then(dados => res.jsonp(dados)) 
+      .catch(erro => res.jsonp(erro))
+  })
+
+  router.get('/lasttimestamps/:id', (req, res) => {
+    connection.getLastTimestamps(req.params.id)
+      .then(dados => res.jsonp(dados))
+      .catch(erro => res.jsonp(erro))
+  })
+
+  router.get('/getDataFilesTablespace/:id/:id2', (req, res) => {
+    connection.getDataFilesTablespace(req.params.id, req.params.id2)
+      .then(dados => res.jsonp(dados))
+      .catch(erro => res.jsonp(erro))
+  })
+
+  router.get('/getDataFiles',(req,res) =>{
+    connection.getDataFiles()
+      .then(dados => res.jsonp(dados)) 
+      .catch(erro => res.jsonp(erro))
+  })
 
   module.exports=router;    
   
